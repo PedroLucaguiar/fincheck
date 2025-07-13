d2c307a69f88f6ce949f3263e8741c87a05bb05a
Pedro Lucas Aguiar <101229772+PedroLucaguiar@users.noreply.github.com>
2025-07-13 21:52:39 +0000

    funcionando a autenticação ++++

--- api/src/modules/auth/auth.service.ts
+++ api/src/modules/auth/auth.service.ts
@@ -1,9 +1,17 @@
 import { Injectable } from '@nestjs/common';
 import { AuthenticateDto } from './dto/authenticate.dto';
+import { UsersRepository } from 'src/shared/database/repositories/users.repositories';
 
 @Injectable()
 export class AuthService {
+    constructor(private readonly usersRepo: UsersRepository) {}
+
     async authenticate( authenticateDto: AuthenticateDto) {
-        return { authenticateDto }
+        const {email} = authenticateDto;
+
+        const user = await this.usersRepo.findUnique({
+            where: {email}
+        });
+        return { user };
     }
 }
